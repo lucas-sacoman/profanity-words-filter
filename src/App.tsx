20ae3f887fe6dd5ff022a98b68a7897ffa4e0ae3
lucas-sacoman <lucas.sousa@somoseducacao.com.br>
2023-03-31 11:29:42 -0300

    adding some styles

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,10 @@ function App() {
 	const handleFetchGptData = (e: FormEvent) => {
 		e.preventDefault();
 
+		if (inputValue.trim() === '') {
+			return;
+		}
+
 		setLoading(true);
 
 		fetch('https://api.openai.com/v1/completions', {
@@ -40,23 +44,29 @@ function App() {
 	};
 
 	return (
-		<main className="bg-slate-900 text-gray-50 flex flex-col justify-center items-center w-screen h-screen">
-			<div className="flex flex-col justify-center items-center gap-4 w-full">
+		<main className="bg-gray-950 text-gray-50 flex flex-col justify-center items-center w-screen h-screen">
+			<div className="flex flex-col justify-center items-center gap-4 w-full max-w-md">
+				<h1 className="mb-12 text-3xl">Filtro de palavrões</h1>
+
 				<form
 					className="flex flex-col justify-center items-center gap-4 w-full h-full"
 					onSubmit={handleFetchGptData}
 				>
 					<textarea
-						className="w-[400px] p-2 border text-slate-900"
-						placeholder="Digite aqui seu texto"
+						className="w-full p-2 border bg-gray-700 rounded text-gray-50 caret-teal-700"
+						placeholder="Digite aqui seu texto com palavrões aqui"
 						name="profanity"
 						id="profanity"
 						value={inputValue}
 						onChange={e => setInputValue(e.target.value)}
 					/>
 
-					<button className="border px-4 py-2" type="submit">
-						enviar
+					<button
+						className="border px-4 py-2 w-full rounded transition hover:bg-cyan-600 disabled:cursor-not-allowed disabled:bg-transparent disabled:opacity-30"
+						type="submit"
+						disabled={inputValue.trim() === ''}
+					>
+						Enviar
 					</button>
 				</form>
 
